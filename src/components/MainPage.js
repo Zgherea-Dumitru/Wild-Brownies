c3debe36868d7410615c6425d0b8a34d6cb3a533
Dumitru <ru.dumit@gmail.com>
2021-05-11 16:44:10 +0200

    planets + hover

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -7,51 +7,34 @@ export default function MainPage() {
         console.log(planet);
     }, [planet])
 
-    const handlePlanetChange = (planetName, e) => {
-        e.stopPropagation();
-        setPlanet(planetName);
-    }
-
-    const handleMouseOver = (e) => {
-        e.stopPropagation();
-    }
-
 
     return (
         <section className="solar-system">
-            <div className="planet neptune" onClick={(e) => handlePlanetChange("neptune", e)}>
-                <div className="img-planet img-neptune"></div>
+            <div className="planet neptune" onClick={(e) => setPlanet("neptune")}>
+                <div className="img-planet img-neptune"></div></div>
 
-                <div className="planet uranus" onClick={(e) => handlePlanetChange("uranus", e)}>
-                    <div className="img-planet img-uranus"></div>
+            <div className="planet uranus" onClick={(e) => setPlanet("uranus")}>
+                <div className="img-planet img-uranus"></div></div>
 
-                    <div className="planet saturn" onClick={(e) => handlePlanetChange("saturn", e)} onMouseEnter={e => handleMouseOver(e)} onMouseLeave={e => handleMouseOver(e)}>
-                        <div className="img-planet img-saturn"></div>
+            <div className="planet saturn" onClick={(e) => setPlanet("saturn")} >
+                <div className="img-planet img-saturn"></div></div>
 
-                        <div className="planet jupiter" onClick={(e) => handlePlanetChange("jupiter", e)}>
-                            <div className="img-planet img-jupiter"></div>
+            <div className="planet jupiter" onClick={(e) => setPlanet("jupiter")}>
+                <div className="img-planet img-jupiter"></div></div>
 
-                            <div className="planet mars" onClick={(e) => handlePlanetChange("mars", e)}>
-                                <div className="img-planet img-mars"></div>
+            <div className="planet mars" onClick={(e) => setPlanet("mars")}>
+                <div className="img-planet img-mars"></div></div>
 
-                                <div className="planet earth" onClick={(e) => handlePlanetChange("earth", e)}>
-                                    <div className="img-planet img-earth"></div>
+            <div className="planet earth" onClick={(e) => setPlanet("earth")}>
+                <div className="img-planet img-earth"></div></div>
 
-                                    <div className="planet venus" onClick={(e) => handlePlanetChange("venus", e)}>
-                                        <div className="img-planet img-venus"></div>
+            <div className="planet venus" onClick={(e) => setPlanet("venus")}>
+                <div className="img-planet img-venus"></div></div>
 
-                                        <div className="planet mercury" onClick={(e) => handlePlanetChange("mercury", e)}>
-                                            <div className="img-planet img-mercury"></div>
+            <div className="planet mercury" onClick={(e) => setPlanet("mercury")}>
+                <div className="img-planet img-mercury"></div></div>
 
-                                            <div className="sun" onClick={(e) => handlePlanetChange("sun", e)}>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div className="sun" onClick={(e) => setPlanet("sun")}>
             </div>
         </section>
     )
